168c4a2c8f091d3f5c922389f390a8551072d6a7
Jon Carr <jwcarr@users.noreply.github.com>
2015-03-22 22:44:59 +0000

    Assign appropriate correlation function to Correlate variable
    Avoids if statements on every call to Correlate() and having to pass
    the correlation_method every time. The relevant correlation function
    from the SciPy stats module is now assigned to the global variable
    Correlate at the start of the Mantel Test run.

--- MantelTest.py
+++ MantelTest.py
@@ -11,29 +11,14 @@ from scipy import array, random, spatial, stats, zeros
 #   significance of the veridical correlation, and a p-value for a normality
 #   test on the distribution of sample correlations (norm).
 
-def MantelTest(distances1, distances2, randomizations=10000, correlation_measure='pearson'):
-  ValidateInput(distances1, distances2, randomizations, correlation_measure)
+def MantelTest(distances1, distances2, randomizations=10000, correlation_method='pearson'):
+  ValidateInput(distances1, distances2, randomizations, correlation_method)
   vector1 = array(distances1, dtype=float)
   vector2 = array(distances2, dtype=float)
-  r, p = Correlate(vector1, vector2, correlation_measure)
-  m, sd, norm = MonteCarlo(vector1, vector2, randomizations, correlation_measure)
+  r, p = Correlate(vector1, vector2)
+  m, sd, norm = MonteCarlo(vector1, vector2, randomizations)
   z = (r-m)/sd
   return r, p, m, sd, z, norm
-
-
-
-# Correlate()
-#   Correlates two vectors using a given type of correlation coefficient
-
-def Correlate(vector1, vector2, correlation_measure):
-  if correlation_measure == 'pearson':
-    return stats.pearsonr(vector1, vector2)
-  elif correlation_measure == 'spearman':
-    return stats.spearmanr(vector1, vector2)
-  elif correlation_measure == 'kendall':
-    return stats.kendalltau(vector1, vector2)
-  else:
-    raise ValueError('The correlation_measure should be set to "pearson", "spearman", or "kendall"')
 
 
 
@@ -43,11 +28,11 @@ def Correlate(vector1, vector2, correlation_measure):
 #   standard deviation of the correlations, and a p-value for a normality test
 #   of the distribution of correlations.
 
-def MonteCarlo(vector1, vector2, randomizations, correlation_measure):
+def MonteCarlo(vector1, vector2, randomizations):
   correlations = zeros(randomizations, dtype=float)
   vector2_as_matrix = spatial.distance.squareform(vector2, 'tomatrix')
   for i in xrange(0, randomizations):
-    correlations[i] = Correlate(vector1, MatrixShuffle(vector2_as_matrix), correlation_measure)[0]
+    correlations[i] = Correlate(vector1, MatrixShuffle(vector2_as_matrix))[0]
   return correlations.mean(), correlations.std(), stats.normaltest(correlations)[1]
 
 
@@ -65,11 +50,10 @@ def MatrixShuffle(matrix):
 # ValidateInput()
 #   Validates input arguments and raises an error if a problem is identified.
 
-def ValidateInput(distances1, distances2, randomizations, correlation_measure):
+def ValidateInput(distances1, distances2, randomizations, correlation_method):
+  global Correlate
   if type(randomizations) != int:
     raise ValueError('The number of randomizations should be an integer')
-  if type(correlation_measure) != str or correlation_measure not in ['pearson', 'spearman', 'kendall']:
-    raise ValueError('The correlation_measure should be set to "pearson", "spearman", or "kendall"')
   if type(distances1) != list or type(distances2) != list:
     raise ValueError('The sets of pairise distances should be Python lists')
   if len(distances1) != len(distances2):
@@ -78,3 +62,11 @@ def ValidateInput(distances1, distances2, randomizations, correlation_measure):
     raise ValueError('The first set of pairwise distances is invalid')
   if spatial.distance.is_valid_y(array(distances2, dtype=float)) == False:
     raise ValueError('The second set of pairwise distances is invalid')
+  if correlation_method == 'pearson':
+    Correlate = stats.pearsonr
+  elif correlation_method == 'spearman':
+    Correlate = stats.spearmanr
+  elif correlation_method == 'kendall':
+    Correlate = stats.kendalltau
+  else:
+    raise ValueError('The correlation method should be set to "pearson", "spearman", or "kendall"')
