4bd3384b2f8949f758094847e93ed694961c9090
Jon Carr <jwcarr@users.noreply.github.com>
2021-01-31 17:14:14 +0100

    Added redundant DM example

--- example.py
+++ example.py
@@ -1,8 +1,23 @@
 import mantel
 
-dists1 = [0.2, 0.4, 0.3, 0.6, 0.9, 0.4] # E.g. genetic distances
-dists2 = [0.3, 0.3, 0.2, 0.7, 0.8, 0.3] # E.g. geographical distances
+# Example with condendensed distance matrices
 
-result = mantel.test(dists1, dists2, perms=10000, method='pearson', tail='upper')
+dists1 = [0.2, 0.4, 0.3, 0.6, 0.9, 0.4]
+dists2 = [0.3, 0.3, 0.2, 0.7, 0.8, 0.3]
 
+result = mantel.test(dists1, dists2, method="pearson", tail="upper")
+print(result)
+
+# Example with redundant distance matrices
+
+dists1 = [[0.0,0.2,0.4,0.3],
+          [0.2,0.0,0.6,0.9],
+          [0.4,0.6,0.0,0.4],
+          [0.3,0.9,0.4,0.0]]
+dists2 = [[0.0,0.3,0.3,0.2],
+          [0.3,0.0,0.7,0.8],
+          [0.3,0.7,0.0,0.3],
+          [0.2,0.8,0.3,0.0]]
+
+result = mantel.test(dists1, dists2, method="pearson", tail="upper")
 print(result)
