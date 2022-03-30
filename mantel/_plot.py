9de3869bf507d0456d2f0aa6b016251453aecf42
Jon Carr <4412666+jwcarr@users.noreply.github.com>
2022-03-30 11:00:37 +0200

    Return deleted code

--- mantel/_plot.py
+++ mantel/_plot.py
@@ -1,7 +1,3 @@
-from ._utils import (
-    probability_from_sample as _probability_from_sample,
-    confidence_interval as _confidence_interval,
-)
 import numpy as np
 from scipy import stats
 
@@ -104,9 +100,9 @@ def plot(
     r = correlations[0]
     m = np.mean(correlations)
     s = np.std(correlations)
-    p = _probability_from_sample(value=r, sample=correlations, tail=tail)
+    p = probability_from_sample(value=r, sample=correlations, tail=tail)
 
-    (min_correlation, max_correlation) = _confidence_interval(
+    (min_correlation, max_correlation) = confidence_interval(
         mean=m, std=s, significance_level=significance_level, tail=tail
     )
 
@@ -161,3 +157,88 @@ def plot(
     plot.axvline(x=r, linestyle=":", color=threshold_color)
     plot.annotate("{:.2f}".format(r), xy=(r, 0.9), color=threshold_color)
     return (min_correlation, max_correlation)
+
+
+def probability_from_sample(value, sample, tail):
+    """
+    Return the empirical p-value of the given value according to the
+    given sample and the given tail method.
+    Parameters
+    ----------
+    value : float
+            The value for which the empirical probability is computed
+    sample : array of floats
+            The observed values.
+    tail : str, optional
+            Which tail to test in the calculation of the empirical p-value; either
+            'upper', 'lower', or 'two-tail'.
+    Returns
+    -------
+    p : float
+            Empirical p-value
+    """
+
+    # Check for valid tail parameter.
+    tail = tail.lower()
+    check_tail_method(tail)
+
+    if tail == "upper":
+        p = sum(sample >= value) / len(sample)
+    elif tail == "lower":
+        p = sum(sample <= value) / len(sample)
+    elif tail == "two-tail":
+        p = sum(abs(sample) >= abs(value)) / len(sample)
+
+    return p
+
+
+def confidence_interval(
+    mean=0,
+    std=1,
+    significance_level=0.05,
+    tail="two-tail",
+):
+    """
+    Return the confidence interval in a normal distribution
+    (characterized by its given mean and its given standard deviation)
+    for accepting the null hypothesis with some given significance
+    level and some given tail method.
+    Parameters
+    ----------
+    mean : float, optional
+            The mean of the normal distribution (default: 0).
+    std : float, optional
+            The standard deviation of the normal distribution
+            (default: 1).
+    significance_level : float (between 0 and 1), optional
+            The probability of rejecting the null hypothesis when it
+            is true (default: 0.05).
+    tail : str, optional
+            Which tail to test in the calculation of the confidence
+            interval; either 'upper', 'lower', or 'two-tail' (default:
+            'two-tail').
+    Returns
+    -------
+    lower_bound : float
+            Lower bound of the confidence interval.
+    upper_bound : float
+            Upper bound of the confidence interval.
+    """
+
+    # Check for valid significance level
+    if not (0 < significance_level < 1):
+        raise ValueError("The significance level must be in the range ]0, 1[")
+    # Check for valid tail parameter.
+    tail = tail.lower()
+    check_tail_method(tail)
+
+    if tail == "upper":
+        lower_bound = stats.norm.ppf(1e-10, mean, std)
+        upper_bound = stats.norm.ppf(1 - significance_level, mean, std)
+    elif tail == "lower":
+        lower_bound = stats.norm.ppf(significance_level, mean, std)
+        upper_bound = stats.norm.ppf(1 - 1e-10, mean, std)
+    elif tail == "two-tail":
+        lower_bound = stats.norm.ppf(significance_level / 2, mean, std)
+        upper_bound = stats.norm.ppf(1 - significance_level / 2, mean, std)
+    return (lower_bound, upper_bound)
