0e8bea0651bc5cb7d30ea564e016c3a29a117055
Jon Carr <4412666+jwcarr@users.noreply.github.com>
2022-03-30 10:36:41 +0200

    Add example using MantelResult

--- example.py
+++ example.py
@@ -1,4 +1,6 @@
+import numpy as np
 import mantel
+
 
 # Example with condendensed distance matrices
 
@@ -7,6 +9,7 @@ dists2 = [0.3, 0.3, 0.2, 0.7, 0.8, 0.3]
 
 result = mantel.test(dists1, dists2, method="pearson", tail="upper")
 print(result)
+
 
 # Example with redundant distance matrices
 
@@ -21,3 +24,24 @@ dists2 = [[0.0,0.3,0.3,0.2],
 
 result = mantel.test(dists1, dists2, method="pearson", tail="upper")
 print(result)
+
+
+# Example with random data (implying no correlation)
+
+n_objects = 27
+n_distances = (n_objects**2 - n_objects) // 2
+
+dists1 = np.random.random(n_distances)
+dists2 = np.random.random(n_distances)
+
+result = mantel.test(dists1, dists2)
+
+print(result.r)
+print(result.p)
+print(result.z)
+
+print(result.p < 0.05)
+
+print(result.correlations)
+print(result.mean)
+print(result.std)
