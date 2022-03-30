9987ead05fea06e17ee25e18e9624e25f2ec0f3a
Jon Carr <4412666+jwcarr@users.noreply.github.com>
2022-03-30 10:56:52 +0200

    blacken _plot.py

--- mantel/_plot.py
+++ mantel/_plot.py
@@ -1,29 +1,35 @@
-from ._utils import \
-    probability_from_sample as _probability_from_sample, \
-    confidence_interval as _confidence_interval
+from ._utils import (
+    probability_from_sample as _probability_from_sample,
+    confidence_interval as _confidence_interval,
+)
 import numpy as np
 from scipy import stats
+
 try:
     import matplotlib.pyplot as plt
+
     _has_matplotlib = True
 except ImportError:
     _has_matplotlib = False
 
 
-def plot(correlations, plot,
-                      tail = "two-tail",
-                      significance_level = 0.05,
-                      gaussian_background_color = 'blue',
-                      gaussian_background_alpha = 0.1,
-                      gaussian_color = 'blue',
-                      gaussian_alpha = 0.3,
-                      gaussian_curve_color = 'blue',
-                      gaussian_curve_alpha = 0.3,
-                      hist_fill_color = 'orange',
-                      hist_edge_color = 'green',
-                      hist_alpha = 0.7,
-                      acceptance_color = 'green',
-                      rejection_color = 'red'):
+def plot(
+    correlations,
+    plot,
+    tail="two-tail",
+    significance_level=0.05,
+    gaussian_background_color="blue",
+    gaussian_background_alpha=0.1,
+    gaussian_color="blue",
+    gaussian_alpha=0.3,
+    gaussian_curve_color="blue",
+    gaussian_curve_alpha=0.3,
+    hist_fill_color="orange",
+    hist_edge_color="green",
+    hist_alpha=0.7,
+    acceptance_color="green",
+    rejection_color="red",
+):
     """
     Plot the correlations previously computed (see
     compute_correlations() function) and on the given matplotlib axes,
@@ -91,19 +97,17 @@ def plot(correlations, plot,
             Maximal correlation value to accept the null hypothesis.
     """
     if not _has_matplotlib:
-        raise Exception("In order to produce histograms, you need to install the 'matplotlib' library first.")
+        raise Exception(
+            "In order to produce histograms, you need to install the 'matplotlib' library first."
+        )
 
     r = correlations[0]
     m = np.mean(correlations)
     s = np.std(correlations)
-    p = _probability_from_sample(value = r,
-                                 sample = correlations,
-                                 tail = tail)
+    p = _probability_from_sample(value=r, sample=correlations, tail=tail)
 
     (min_correlation, max_correlation) = _confidence_interval(
-        mean = m, std = s,
-        significance_level = significance_level,
-        tail = tail
+        mean=m, std=s, significance_level=significance_level, tail=tail
     )
 
     x = np.linspace(min_correlation, max_correlation, 100)
@@ -114,19 +118,46 @@ def plot(correlations, plot,
     x_all = np.linspace(lower, upper, 100)
     y_all = stats.norm.pdf(x_all, m, s)
 
-    plot.fill_between(x_all, y_all, 0, color=gaussian_background_color, alpha=gaussian_background_alpha)
+    plot.fill_between(
+        x_all,
+        y_all,
+        0,
+        color=gaussian_background_color,
+        alpha=gaussian_background_alpha,
+    )
     plot.fill_between(x, y, 0, color=gaussian_color, alpha=gaussian_alpha)
     plot.plot(x_all, y_all, color=gaussian_curve_color, alpha=gaussian_curve_alpha)
-    plot.vlines(x=[min_correlation, max_correlation], ymin=0, ymax=[stats.norm.pdf(min_correlation, m, s), stats.norm.pdf(max_correlation, m, s)], linestyle='-', color=gaussian_curve_color, alpha=gaussian_curve_alpha)
+    plot.vlines(
+        x=[min_correlation, max_correlation],
+        ymin=0,
+        ymax=[
+            stats.norm.pdf(min_correlation, m, s),
+            stats.norm.pdf(max_correlation, m, s),
+        ],
+        linestyle="-",
+        color=gaussian_curve_color,
+        alpha=gaussian_curve_alpha,
+    )
 
-    plot.hist(correlations, bins=20, range=(lower, upper), density=True, histtype='stepfilled', color=hist_fill_color, edgecolor=hist_edge_color, alpha=hist_alpha)
+    plot.hist(
+        correlations,
+        bins=20,
+        range=(lower, upper),
+        density=True,
+        histtype="stepfilled",
+        color=hist_fill_color,
+        edgecolor=hist_edge_color,
+        alpha=hist_alpha,
+    )
 
     plot.set_xlim(left=lower, right=upper)
     plot.set_xlabel("correlation coefficients")
 
     plot.set_ylabel("Density")
 
-    threshold_color = acceptance_color if min_correlation <= r <= max_correlation else rejection_color
-    plot.axvline(x=r, linestyle=':', color=threshold_color)
-    plot.annotate('{:.2f}'.format(r), xy=(r, 0.9), color=threshold_color)
+    threshold_color = (
+        acceptance_color if min_correlation <= r <= max_correlation else rejection_color
+    )
+    plot.axvline(x=r, linestyle=":", color=threshold_color)
+    plot.annotate("{:.2f}".format(r), xy=(r, 0.9), color=threshold_color)
     return (min_correlation, max_correlation)
